fe743cb3277484242777ce8932b0a57fe2a29d92
kidnut <kidnut@icloud.com>
2024-06-27 00:52:03 +0300

    Task 1.1.5

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -9,6 +9,17 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
+    private final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS pre_project.users (\n" +
+            "  id BIGINT(200) NOT NULL AUTO_INCREMENT,\n" +
+            "  name VARCHAR(45) NOT NULL,\n" +
+            "  lastName VARCHAR(45) NOT NULL,\n" +
+            "  age TINYINT(3) NOT NULL,\n" +
+            "  PRIMARY KEY (id))";
+    private final String DROP_TABLE = "DROP TABLE IF EXISTS pre_project.users";
+    private final String INSERT_USERS = "INSERT INTO pre_project.users (name, lastName, age) VALUES (?, ?, ?)";
+    private final String SELECT_ALL = "SELECT * FROM pre_project.users";
+    private final String DELETE = "DELETE FROM pre_project.users WHERE id = ? IS NULL OR id = ''";
+    private final String CLEAN = "TRUNCATE TABLE pre_project.users";
 
     @Override
     public void createUsersTable() {
